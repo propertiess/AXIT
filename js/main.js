211c091a98a61c7d5d2e7cfb7e0750000dc5a3da
propertiess <erofeev2001@yandex.ru>
2022-06-22 15:37:45 +0300

    Fix the mobile-menu

--- js/main.js
+++ js/main.js
@@ -1,13 +1,41 @@
-const menu = document.querySelector('.navbar-mobile__btn__image img')
+const menu = document.querySelector('.navbar-mobile__btn__image')
+const menuImg = document.querySelector('.navbar-mobile__btn__image img')
 const hiddenMenu = document.querySelector('.navbar__aside')
-console.log(menu);
+const wrapper = document.querySelector('.wrapper')
+console.log(wrapper);
+let menuMobileOpen = false
 menu.addEventListener('click', () => {
-    hiddenMenu.style.display = 'flex'
-    hiddenMenu.style.visibility = 'visible'
+    if(!menuMobileOpen){
+        menuMobileOpen = true
 
+    }
+    else {
+        wrapper.style = 'max-height: none'
 
-    setTimeout(() => {
-        menu.style.filter = 'brightness(5)'
-    }, 500)
+        hiddenMenu.style.display = 'none'
+        hiddenMenu.style.visibility = 'hidden'
+        menuMobileOpen = false
+        setTimeout(() => {
+            menuImg.style.filter = 'brightness(5)'
+        }, 300)
+    }
+    if(menuMobileOpen) {
+        hiddenMenu.style.display = 'flex'
+        hiddenMenu.style.visibility = 'visible'
+        wrapper.style = 'max-height: 200px'
     
+        // setTimeout(() => {
+        //     menu.style.filter = 'brightness(5)'
+        // }, 300)
+    }
+    
+    
+})
+hiddenMenu.addEventListener('click', () => {
+    wrapper.style = 'max-height: none'
+
+    hiddenMenu.style.display = 'none'
+    hiddenMenu.style.visibility = 'hidden'
+    menuMobileOpen = false
+
 })
