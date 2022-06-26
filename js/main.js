f9631c43162de0014aa6039a765eb49796ecc080
propertiess <erofeev2001@yandex.ru>
2022-06-26 11:40:15 +0300

    Add animation

--- js/main.js
+++ js/main.js
@@ -1,46 +1,61 @@
 const menu = document.querySelector('.navbar-mobile__btn__image')
-const menuImg = document.querySelector('.navbar-mobile__btn__image img')
+const menuImg = document.querySelector('.image')
 const hiddenMenu = document.querySelector('.navbar__aside')
 const wrapper = document.querySelector('.wrapper')
-console.log(wrapper);
+
+console.log(hiddenMenu);
+console.log(menuImg);
 let menuMobileOpen = false
 menu.addEventListener('click', () => {
-    if(!menuMobileOpen){
-        menuMobileOpen = true
-        setTimeout(() => {
-            menuImg.style.filter = 'brightness(100)'
-        }, 100)
-    }
-    else {
-        wrapper.style = 'max-height: none'
+    // if(!menuMobileOpen){
+    //     menuMobileOpen = true
+    //     setTimeout(() => {
+    //         menuImg.style.filter = 'brightness(100)'
+    //     }, 100)
+    // }
+    // else {
+    //     wrapper.style = 'max-height: none'
 
-        hiddenMenu.style.display = 'none'
-        hiddenMenu.style.visibility = 'hidden'
-        menuMobileOpen = false
-        setTimeout(() => {
-            menuImg.style.filter = 'brightness(5)'
-        }, 300)
-    }
-    if(menuMobileOpen) {
-        hiddenMenu.style.display = 'flex'
-        hiddenMenu.style.visibility = 'visible'
-        wrapper.style = 'max-height: 200px'
+    //     hiddenMenu.style.display = 'none'
+    //     hiddenMenu.style.visibility = 'hidden'
+    //     menuMobileOpen = false
+    //     setTimeout(() => {
+    //         menuImg.style.filter = 'brightness(5)'
+    //     }, 300)
+    // }
+    // if(menuMobileOpen) {
+    //     hiddenMenu.style.display = 'flex'
+    //     hiddenMenu.style.visibility = 'visible'
+    //     wrapper.style = 'max-height: 200px'
     
         // setTimeout(() => {
         //     menu.style.filter = 'brightness(5)'
         // }, 300)
+
+
+        hiddenMenu.classList.toggle('active')
+        menuImg.classList.toggle('active__btn')
+        wrapper.classList.toggle('active__scroll')
     }
     
     
-})
-hiddenMenu.addEventListener('click', () => {
-    wrapper.style = 'max-height: none'
+)
+// hiddenMenu.addEventListener('click', () => {
+//     wrapper.style = 'max-height: none'
 
-    hiddenMenu.style.display = 'none'
-    hiddenMenu.style.visibility = 'hidden'
-    menuMobileOpen = false
-    setTimeout(() => {
-        menuImg.style.filter = 'brightness(5)'
-    }, 300)
+//     hiddenMenu.style.display = 'none'
+//     hiddenMenu.style.visibility = 'hidden'
+//     menuMobileOpen = false
+//     setTimeout(() => {
+//         menuImg.style.filter = 'brightness(5)'
+//     }, 300)
 
-})
+// })
+// btnSign.addEventListener('click', () => {
+//     if (nameSign.value === '')
+//         var error = document.createElement('div')
+//         error.className='error'
+//         error.style.color = 'red'
+//         error.innerHTML = 'Cannot be blank'
+//         document.innerHTML = error
+// })
